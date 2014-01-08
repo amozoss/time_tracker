f10d313ce622e22dd20a5662eca201b8dd3c5889
Dan Willoughby <amozoss@gmail.com>
2014-01-08 15:26:44 -0700

    changing path

--- time_tracker.pl
+++ time_tracker.pl
@@ -46,7 +46,7 @@ my $reportHours;
 my $reportChargeCode;
 my $reportDescription;
 my $reportListIndex;
-my $filename = "./time_card";
+my $filename = "/home/dan/Documents/time_tracker/time_card";
  
  
 my $timeCard;# Stores each days data
